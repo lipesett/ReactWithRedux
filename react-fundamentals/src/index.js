7203395a55588037cd7a017ac55a8fded1a93b38
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-13 23:40:41 -0300

    adding React to enable JSX usage

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,5 +1,13 @@
 // DOCUMENT OBJECT MODEL = DOM
 import ReactDOM from "react-dom";
 
+// ADDING REACT TO ENABLE JSX USAGE
+import React from "react";
+
 const element = document.getElementById('root');
-ReactDOM.render('Hello, React!!', element);
+const tag = <strong>Hello, React!</strong>
+ReactDOM.render(
+    <div>
+        {tag}
+    </div>,
+    element);
