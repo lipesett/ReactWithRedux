d9c3f0169bfd88f06041afaec303cdfd6c528137
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-13 23:48:32 -0300

    creating and importing the CSS file

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,8 +1,10 @@
 // DOCUMENT OBJECT MODEL = DOM
 import ReactDOM from "react-dom";
-
 // ADDING REACT TO ENABLE JSX USAGE
 import React from "react";
+// ADDING CSS
+import './index.css'
+
 
 const element = document.getElementById('root');
 const tag = <strong>Hello, React!</strong>
